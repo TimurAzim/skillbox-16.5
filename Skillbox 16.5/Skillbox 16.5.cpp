3cef481f6d0a05f59f3d2e932dc2aa67ce9d27db
Timur Azim <papaas73@gmail.com>
2024-03-03 22:03:36 +0600

    Skillbox 16.5

---
+++ Skillbox 16.5/Skillbox 16.5.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <ctime>
+
+int main() {
+    const int N = 5; 
+    int array[N][N]; 
+
+    std::cout << "Array:" << std::endl;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            array[i][j] = i + j;
+            std::cout << array[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    time_t now = time(0);
+    struct tm localTime;
+    localtime_s(&localTime, &now);
+
+    int currentDay = localTime.tm_mday;
+    int rowIndex = currentDay % N;
+    int sum = 0;
+    for (int j = 0; j < N; j++) {
+        sum += array[rowIndex][j];
+    }
+
+    std::cout << "Sum of elements in row " << rowIndex << ": " << sum << std::endl;
+
+    return 0;
+}
